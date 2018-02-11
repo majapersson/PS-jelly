a61719418640d019932eed4bd6d6eb583b28f777
Maja Persson <31646734+majapersson@users.noreply.github.com>
2018-02-11 17:22:23 +0100

    Add files via upload

--- PS-Jelly/PS-Jelly.ps1
+++ PS-Jelly/PS-Jelly.ps1
@@ -49,104 +49,130 @@ function Print-SystemLine {
     Print-System -string "`n===================="
 }
 
-Clear-Host
+$previousColor = [console]::ForegroundColor
 
-$hero = Get-Content $PSScriptRoot/Jelly-header.ps1
+try {
 
-echo $hero
+    Clear-Host
 
-Print-Color -color "Cyan" -output "Made by Maja Persson"
-Print-Color -color "Magenta" -output "http://github.com/majapersson`n"
-Print-Color -color "Cyan" -output "Peanut Butter for Mac by Andreas Lundqvist available at"
-Print-Color -color "Magenta" -output "http://github.com/momentiris`n"
+    $hero = Get-Content $PSScriptRoot/Jelly-header.ps1
 
-Print-SystemLine
-Print-System -string "Initilizing package.json file..."
+    echo $hero
 
-npm init
+    Print-Color -color "Cyan" -output "Made by Maja Persson"
+    Print-Color -color "Magenta" -output "http://github.com/majapersson`n"
+    Print-Color -color "Cyan" -output "Peanut Butter for Mac by Andreas Lundqvist available at"
+    Print-Color -color "Magenta" -output "http://github.com/momentiris`n"
 
-
-if (!(Test-Path ./.git/)) {
     Print-SystemLine
-    Print-Color -color "Red" -output "This directory does not seem to be a git repository."
-    $gitInit = Read-System -string "Do you want to initialize it as a git repo? (Y/N)"
+    Print-System -string "Initilizing package.json file..."
 
-    if ($gitInit -like "Y") {
-        git init
-    }
-}
+    npm init
 
-if (Test-Path ./.git/) {
-    
-    Print-SystemLine
-    $createGitIgnore = Read-System -string "Create .gitignore? (Y/N)"
 
-    if ($createGitIgnore -like "Y") {
-        Print-System -string "Creating .gitignore..."
-        New-Item .gitignore
-        echo node_modules >> .gitignore
-        if (Test-Path ./public/) {
-            echo public/assets >> .gitignore
+    if (!(Test-Path ./.git/)) {
+        Print-SystemLine
+        Print-Color -color "Red" -output "This directory does not seem to be a git repository."
+        $gitInit = Read-System -string "Do you want to initialize it as a git repo? (Y/N)"
+
+        if ($gitInit -like "Y") {
+            git init
         }
     }
-}
 
-Print-SystemLine
-$packageString = Read-System -string "Print packages to install separated by spaces"
-
-$packages = $packageString -split ' '
-
-
-For($i=0; $i -lt $packages.length; $i++){
+    if (Test-Path ./.git/) {
     
-    Print-SystemLine
-    Print-System -string "Installing $($packages[$i])"
+        Print-SystemLine
+        $createGitIgnore = Read-System -string "Create .gitignore? (Y/N)"
 
-    npm i $packages[$i]
-}
-
-Print-SystemLine
-$createStructure = Read-System -string "Create public/assets and src file structure? (Y/N)"
-
-if ($createStructure -like "Y") {
-
-    Print-SystemLine
-    Print-System -string "Creating file structure..."
-
-    mkdir public/assets/scripts
-    mkdir public/assets/styles
-
-    mkdir src/scripts
-    mkdir src/styles
-
-    New-Item src/scripts/index.js
-    New-Item src/styles/main.scss
-
-    New-Item public/index.html
-
-}
-
-if($packages.Contains("rollup")) {
+        if ($createGitIgnore -like "Y") {
+            Print-System -string "Creating .gitignore..."
+            New-Item .gitignore
+            echo node_modules | Out-File .gitignore -Encoding utf8 -Append
+        }
+    }
 
     Print-SystemLine
-    $createRollupConfig = Read-System -string "Create rollup.config file? (Y/N)"
+    $packageString = Read-System -string "Print packages to install separated by spaces"
 
-    if ($createRollupConfig -like "Y") {
+    $packages = $packageString -split ' '
+
+
+    For($i=0; $i -lt $packages.length; $i++){
+    
+        Print-SystemLine
+        Print-System -string "Installing $($packages[$i])"
+
+        npm i $packages[$i]
+    }
+
+    Print-SystemLine
+    $createStructure = Read-System -string "Create public/assets and src file structure? (Y/N)"
+
+    if ($createStructure -like "Y") {
+
+        Print-SystemLine
+        Print-System -string "Creating file structure..."
+
+        mkdir public/assets/scripts
+        mkdir public/assets/styles
+
+        mkdir src/scripts
+        mkdir src/styles
+
+        New-Item src/scripts/index.js        
+
+        if ($packages.Contains("node-sass")) {
+            mkdir src/styles/components
+            New-Item src/styles/components/__components.scss
+            mkdir src/styles/project
+            New-Item src/styles/project/__project.scss
+            New-Item src/styles/main.scss
+
+            echo '@import "project/_project";' | Out-File src/styles/main.scss -Encoding utf8
+            echo '@import "components/_components";' | Out-File src/styles/main.scss -Encoding utf8 -Append
+        } else {
+            New-Item src/styles/main.css
+        }
+
+        New-Item public/index.html
+        Get-Content $PSScriptRoot/index.html | Out-File public/index.html -Encoding utf8
+
+        if ((Test-Path ./public/) -And ($createGitIgnore -like "Y")){
+            echo public/assets/*| Out-File .gitignore -Encoding utf8 -Append
+        }
+
+    }
+
+    if($packages.Contains("rollup")) {
+
+        Print-SystemLine
+        $createRollupConfig = Read-System -string "Create rollup.config file? (Y/N)"
+
+        if ($createRollupConfig -like "Y") {
         
-        Print-System -string "Creating rollup.config.js..."
-        New-Item rollup.config.js
+            Print-System -string "Creating rollup.config.js..."
+            New-Item rollup.config.js
 
-        cat $PSScriptRoot/rollup.config.js >> ./rollup.config.js
+             $rollupContent = Get-Content -Raw $PSScriptRoot/rollup.config.js 
+             echo $rollupContent | Out-File ./rollup.config.js -Encoding utf8
+        }
+    }
+
+    Print-SystemLine
+    $createScripts = Read-System -string "Install json package globally and copy scripts from scripts.json? (Y/N)"
+
+    if ($createScripts -like "Y") {
+
+        npm i json -g
+    
+        Print-System -string "Copying scripts from scripts.json to package.json..."
+        $scripts = cat $PSScriptRoot/scripts.json
+
+        json -I -f ./package.json -e "this.scripts={${scripts}}"
     }
 }
 
-Print-SystemLine
-$createScripts = Read-System -string "Copy scripts from scripts.json file? (Y/N)"
-
-if ($createScripts -like "Y") {
-    
-    Print-System -string "Copying scripts from scripts.json to package.json..."
-    $scripts = cat $PSScriptRoot/scripts.json
-
-    json -I -f ./package.json -e "this.scripts={${scripts}}"
+finally {
+    [console]::ForegroundColor = $previousColor
 }
